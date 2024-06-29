4660efac228e50f7123408b4eb000bcc767a6c5d
saitoi <pedrohhs@dcc.ufrj.br>
2024-06-29 17:48:26 -0300

    Removendo configurations.ini

--- back/main.py
+++ back/main.py
@@ -1,22 +1,9 @@
-import argparse
 import uvicorn
-import configparser
 import os.path as p
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--config", help="Configuration file", type=str, default=p.join(p.dirname(p.abspath(__file__)), 'configurations.ini'))
-args = parser.parse_args()
-
-if args.config:
-    print("Starting service with config file: ", args.config)
-
-config = configparser.ConfigParser()
-config.read(args.config)
 
 def main():
     # Prepare application environment:
     uvicorn_env_file = open(p.join(p.dirname(p.abspath(__file__)), '.uvicorn.env'), "w")
-    uvicorn_env_file.write(f"databaseConnectionString={config['database']['ConnectionString']}")
     uvicorn_env_file.close()
 
     # Start server: 
