8e051e8abdf5322f91ec3300ad600f8e4ae56be4
milton-salgado <miltonsalgadoleandro@gmail.com>
2024-07-02 19:38:35 -0300

    refact: Refatoração do componente de Dashboard

--- front/src/components/Dashboard/index.js
+++ front/src/components/Dashboard/index.js
@@ -4,11 +4,11 @@ import ChartCountGamesPlatforms from "../Chart/ChartCountGamesPlatforms";
 import ChartCountGamesStores from "../Chart/ChartCountGamesStores/index.js";
 import ChartCountGamesPublishers from "../Chart/ChartCountGamesPublishers/index.js";
 import ChartCountGamesGenders from "../Chart/ChartCountGamesGenders/index.js";
-import GamesPlatformsTable from "../GamesPlatformsTable/index.js";
+import GamesPlatformsTable from "../Table/GamesPlatformsTable/index.js";
 import ChartAvgGenderAvaliation from "../Chart/ChartAvgGenderAvaliation/index.js";
-import { StyledContainer } from "./styles";
-import GamesDeveloperTable from "../GamesDevelopersTable/index.js";
-import CreatorsRolesTable from "../CreatorsRolesTable/index.js";
+import { StyledContainer } from "./styles.js";
+import GamesDeveloperTable from "../Table/GamesDevelopersTable/index.js";
+import CreatorsRolesTable from "../Table/CreatorsRolesTable/index.js";
 
 const Dashboard = () => {
     return (
--- front/src/components/Dashboard/styles.js
+++ front/src/components/Dashboard/styles.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const StyledContainer = styled.div`
     display: flex;
     flex-direction: column;
+    gap: 40px;
     padding: 20px;
 `;
